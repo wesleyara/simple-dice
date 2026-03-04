d2babdf631beca1b165c9c88f22eb7d0141c585f
Wesley Araújo <89321125+wesleyara@users.noreply.github.com>
2026-03-04 13:46:42 -0300

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,54 @@
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import perfectionist from "eslint-plugin-perfectionist";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { ignores: ["node_modules/", "dist/", "build/", "coverage/", "lib/"] },
+  {
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+  },
+  pluginJs.configs.recommended,
+  eslintPluginPrettier,
+  eslintConfigPrettier,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      perfectionist,
+    },
+    rules: {
+      "no-console": "off",
+      "no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+      "prettier/prettier": [
+        "error",
+        {
+          singleQuote: false,
+          tabWidth: 2,
+          semi: true,
+          trailingComma: "all",
+          printWidth: 100,
+          endOfLine: "auto",
+          arrowParens: "avoid",
+        },
+      ],
+      "perfectionist/sort-imports": "error",
+      "perfectionist/sort-modules": "error",
+      "perfectionist/sort-named-imports": "error",
+      "perfectionist/sort-union-types": "error",
+    },
+  },
+];
